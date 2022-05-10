f21dcf3b2b5b1ab222b5a9e86bd386511a126a58
Gustavo Bretas <gustavobretas@gmail.com>
2022-05-10 02:18:55 +0000

    Render links to our posts

--- remix-blog/app/routes/posts/index.tsx
+++ remix-blog/app/routes/posts/index.tsx
@@ -1,8 +1,16 @@
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 
+type Post = {
+    slug: string;
+    title: string;
+  };
+
+type LoaderData = {
+    posts: Array<Post>;
+  };
 export const loader = async () => {
-  return json({
+  return json<LoaderData>({
     posts: [
       {
         slug: "my-first-post",
@@ -17,11 +25,23 @@ export const loader = async () => {
 };
 
 export default function Posts() {
-  const { posts } = useLoaderData();
-  console.log(posts);
-  return (
+    const { posts } = useLoaderData() as LoaderData;
+    console.log(posts);
+    return (
     <main>
-      <h1>Posts</h1>
+        <h1>Posts</h1>
+        <ul>
+        {posts.map((post) => (
+            <li key={post.slug}>
+            <Link
+                to={post.slug}
+                className="text-blue-600 underline"
+            >
+                {post.title}
+            </Link>
+            </li>
+        ))}
+        </ul>
     </main>
-  );
+    );
 }
