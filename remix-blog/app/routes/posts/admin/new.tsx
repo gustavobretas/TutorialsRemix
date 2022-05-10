3777e1325c5f22182dcaf4b98fb6a7111032d964
Gustavo Bretas <gustavobretas@gmail.com>
2022-05-10 03:12:17 +0000

    Add some pending UI with useTransition

--- remix-blog/app/routes/posts/admin/new.tsx
+++ remix-blog/app/routes/posts/admin/new.tsx
@@ -1,6 +1,6 @@
 import type { ActionFunction } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form, useActionData } from "@remix-run/react";
+import { Form, useActionData, useTransition } from "@remix-run/react";
 
 import { createPost } from "~/models/post.server";
 
@@ -47,7 +47,7 @@ export const action: ActionFunction = async ({
     typeof markdown === "string",
     "markdown must be a string"
   );
-  
+
   await createPost({ title, slug, markdown });
 
   return redirect("/posts/admin");
@@ -57,6 +57,10 @@ const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`
 
 export default function NewPost() {
   const errors = useActionData();
+
+  const transition = useTransition();
+  const isCreating = Boolean(transition.submission);
+
   return (
     <Form method="post">
       <p>
@@ -105,8 +109,9 @@ export default function NewPost() {
         <button
           type="submit"
           className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+          disabled={isCreating}
         >
-          Create Post
+          {isCreating ? "Creating..." : "Create Post"}
         </button>
       </p>
     </Form>
